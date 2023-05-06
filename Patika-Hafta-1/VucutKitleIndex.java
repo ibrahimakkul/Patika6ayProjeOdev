d5fb834aaa8928706b6fdf2eb547a40e5bdf6079
İbrahim <102255701+ibrahimakkul@users.noreply.github.com>
2023-05-06 04:04:23 +0300

    Add files via upload

---
+++ Patika-Hafta-1/VucutKitleIndex.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen boyunuzu (metre cinsinden) girin: ");
+        double boy = input.nextDouble();
+
+        System.out.print("Lütfen kilonuzu (kilogram cinsinden) girin: ");
+        double kilo = input.nextDouble();
+
+        double vki = kilo / (boy * boy);
+
+        System.out.println("Vücut Kitle İndeksiniz: " + vki);
+    }
+}
